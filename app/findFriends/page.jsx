f275f6c6fc66c287a535eb9a539ce76ed0810e7e
YoYoDough <yosuf818s@gmail.com>
2024-11-21 22:32:26 -0800

    Updated find users page.

--- app/findFriends/page.jsx
+++ app/findFriends/page.jsx
@@ -7,23 +7,31 @@ const page = () => {
     const [searchResults, setSearchResults] = useState([]); // Filtered users
     const [allUsers, setAllUsers] = useState([]); // Full list of users
     const [showDropdown, setShowDropdown] = useState(false);
+    console.log(searchResults);
 
-    const fetchUsersFromInput = async(e) => {
-      const response = await fetch("http://localhost:8080/api/users");
-      const data = response.json();
-      const result = data.filter((user) => {
-        return (
-          e &&
-          user &&
-          user.name &&
-          user.name.toLowerCase().includes(e)
-        )
-      })
-
+    const fetchUsersFromInput = async(input) => {
+        if (!input.trim() || input.length === 0){
+          setSearchResults([]);
+          return;
+        }
+        console.log(input);
+        const response = await fetch("http://localhost:8080/api/users/input");
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+    
+        const result = data.filter((user) => {
+          return (
+            user.name &&
+            user.name.toLowerCase().includes(input.toLowerCase())
+          );
+        });
+        setSearchResults(result);
     }
 
     const handleChange = (e) =>{
         setInput(e.target.value);
+        fetchUsersFromInput(input)
     }
   return (
     <div className  = "flex flex-col p-10 m-10">
@@ -31,6 +39,13 @@ const page = () => {
         <input className = "text-black w-30 h-10 mt-5 p-2"type = "text" onChange = {handleChange} placeholder = "Type in your friends correct name along with # tag">
 
         </input>
+        <div className="mt-2 bg-white border rounded shadow">
+          {searchResults.map((user) => (
+            <div key={user.id} className="p-2 hover:bg-gray-200">
+              {user.name}
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
