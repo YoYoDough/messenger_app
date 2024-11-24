4b6756deb97916ad4f948e5de697d2b31b1aab49
YoYoDough <yosuf818s@gmail.com>
2024-11-23 21:58:34 -0800

    Updated friends page.

--- app/findFriends/page.jsx
+++ app/findFriends/page.jsx
@@ -14,15 +14,32 @@ const page = () => {
 
     console.log(searchResults);
 
-    const handleFriendAdd = async() => {
+    let userId;
+    console.log(userId);
 
+    const getUsersId = async(userId) => {
+      //get user id from session email
+    }
+
+    const handleFriendAdd = async(user, userId) => {
+      const response = await fetch("http://localhost:8080/api/friends", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: {
+          user_id: userId,
+          friend_id: user.id,
+          status: "pending",
+        }
+      })
     }
 
     const handleChatClick = () => {
 
     }
 
-    const fetchUsersFromInput = async(input) => {
+    const fetchUsersFromInput = async(input, userId) => {
         if (!input.trim() || input.length === 0){
           setSearchResults([]);
           return;
@@ -32,6 +49,11 @@ const page = () => {
         console.log(response);
         const data = await response.json();
         console.log(data);
+
+        data.map((user)=> {
+          user.email === session?.user.email ? userId = user.id: null;
+        })
+        
     
         const result = data.filter((user) => {
           return (
@@ -54,7 +76,7 @@ const page = () => {
         <input className = "text-black w-30 h-10 mt-5 p-2"type = "text" onChange = {handleChange} placeholder = "Type in the user's correct name along with # tag">
 
         </input>
-        <div className="mt-2 bg-white border rounded shadow">
+        <div className="mt-2 max-h-72 bg-white border rounded shadow">
           {searchResults.map((user) => (
             <div key={user.id} className="flex justify-between items-center text-black p-2 hover:bg-gray-200">
               <div className = "flex items-center">
@@ -63,7 +85,7 @@ const page = () => {
               </div>
               
               <div className = "flex justify-center items-center">
-                <button title = "Add as a friend"onClick = {handleFriendAdd} className = "flex w-10 align-self-center hover:bg-gray-400 rounded-full mr-1"><img src = "addFriend.png" alt = "Add friend image"></img></button>
+                <button title = "Add as a friend" onClick = {() => handleFriendAdd(user)} className = "flex w-10 align-self-center hover:bg-gray-400 rounded-full mr-1"><img src = "addFriend.png" alt = "Add friend image"></img></button>
                 <button title = "Send message" onClick = {handleChatClick} className = "flex w-10 align-self-center hover:bg-gray-400 rounded-full p-1"><img src = "sendMessage.png" alt = "Send message image"></img></button>
               </div>
             </div>
